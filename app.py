b6796a1db4063296e4947bc1885852ccbdd826b6
Gita Bhupathiraju <81713502+gita32@users.noreply.github.com>
2026-03-28 17:48:24 -0400

    Initial commit - plant monitor app

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify, render_template
+import serial
+import threading
+
+app = Flask(__name__)
+
+ser = serial.Serial('COM3', 9600)  # CHANGE THIS
+data = {"temp": 0, "moist": 0}
+
+THRESHOLD = 300
+
+def read_serial():
+    global data
+    while True:
+        line = ser.readline().decode().strip()
+
+        if "MOIST" in line:
+            parts = line.split(",")
+            temp = float(parts[0].split(":")[1])
+            moist = int(parts[1].split(":")[1])
+
+            data = {"temp": temp, "moist": moist}
+
+threading.Thread(target=read_serial, daemon=True).start()
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/data")
+def get_data():
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
